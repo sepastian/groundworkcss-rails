bb5be2c637d32ba5066a1d7be43fd2df5cf49860
Gary Hepting and Laura DeWald <engineering+gary+laura@sidereel.com>
2013-06-18 20:51:02 -0700

    fixed up generator install

--- lib/groundworkcss/generators/install_generator.rb
+++ lib/groundworkcss/generators/install_generator.rb
@@ -7,7 +7,7 @@ module GroundworkCSS
 
       def add_assets
         insert_into_file "app/assets/javascripts/application#{detect_js_format[0]}", "#{detect_js_format[1]} require groundworkcss\n", :after => "jquery_ujs\n"
-        insert_into_file "app/assets/stylesheets/application#{detect_css_format[0]}", "#{detect_css_format[1]} require groundworkcss\n", :after => "require_self\n"              
+        insert_into_file "app/assets/stylesheets/application#{detect_css_format[0]}", "@import 'groundworkcss/groundwork';\n", :after => "*/\n"
       end
 
       def detect_js_format
@@ -16,11 +16,11 @@ module GroundworkCSS
       end
 
       def detect_css_format
-        return ['.css', ' *='] if File.exist?('app/assets/stylesheets/application.css')
-        return ['.css.sass', ' //='] if File.exist?('app/assets/stylesheets/application.css.sass')
-        return ['.sass', ' //='] if File.exist?('app/assets/stylesheets/application.sass')
-        return ['.css.scss', ' //='] if File.exist?('app/assets/stylesheets/application.css.scss')
-        return ['.scss', ' //='] if File.exist?('app/assets/stylesheets/application.scss')
+        return ['.css'] if File.exist?('app/assets/stylesheets/application.css')
+        return ['.css.sass'] if File.exist?('app/assets/stylesheets/application.css.sass')
+        return ['.sass'] if File.exist?('app/assets/stylesheets/application.sass')
+        return ['.css.scss'] if File.exist?('app/assets/stylesheets/application.css.scss')
+        return ['.scss'] if File.exist?('app/assets/stylesheets/application.scss')
       end
     end
   end
